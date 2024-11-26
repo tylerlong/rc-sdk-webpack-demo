8fd2f3b7a292a48f111fef81a59e1b9bffaacfae
Tyler Liu <tyler4long@gmail.com>
2024-11-26 10:26:43 -0800

    First working version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 document.body.innerHTML = "<h1>Hello world!</h1>";
+
+const RingCentral = require("@rc-ex/core").default;
+const WSExtension = require("@rc-ex/ws").default;
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN,
+  });
+
+  const ext = new WSExtension();
+  await rc.installExtension(ext);
+  await ext.subscribe(
+    ["/restapi/v1.0/account/~/extension/~/message-store"],
+    (e) => {
+      console.log(e);
+    }
+  );
+  console.log("subscribed");
+};
+main();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin, ProvidePlugin } = require("webpack");
+const dotenv = require("dotenv");
 
 module.exports = {
-  plugins: [new HtmlWebpackPlugin()],
+  resolve: {
+    fallback: {
+      buffer: require.resolve("buffer/"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new DefinePlugin({
+      "process.env": JSON.stringify(dotenv.config().parsed),
+    }),
+    new ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
 };
